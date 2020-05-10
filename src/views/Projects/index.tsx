8dadf663dafd263368538b8240944a7640ed76e3
Dayton Taylor <taylor@civicplus.com>
2020-05-10 12:24:10 -0500

    set document title on Projects page load

--- src/views/Projects/index.tsx
+++ src/views/Projects/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Projects() {
+	useEffect(() => {
+		document.title = 'Dayton Taylor | Projects';
+	});
+
 	return <div></div>;
 }
 
