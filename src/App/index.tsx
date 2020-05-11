d524950d9a3e1bee6efcf7c07cd817ee301a5331
Dayton Taylor <taylor@civicplus.com>
2020-05-11 01:05:23 -0500

    added default route to show NotFound page and redirect from empty path to about page

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
-import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
 import About from '../views/About';
-import Projects from '../views/Projects';
+import NotFound from '../views/NotFound';
 
 window.isMobileView =
 	/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
@@ -33,8 +33,8 @@ const App: React.FC<AppProps> = () => {
 
 			<Switch>
 				<Route exact path="/about" component={About} />
-				<Route exact path="/projects" component={Projects} />
-				<Route render={() => <Redirect to="/about" />} />
+				<Route exact path="/" render={() => <Redirect to="/about" />} />
+				<Route component={NotFound} />
 			</Switch>
 
 			<PageFooter />
